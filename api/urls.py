92279109bbe11a51067a94ad4db2c55f00aeeb87
ssava <santa03777@gmail.com>
2024-05-14 03:01:11 +0300

    uploading menu and getting current day menu functionality

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from api.views import UserCreateAPIView
+from api.views import UserCreateAPIView, MenuListCreateAPIView
 
 urlpatterns = [
     path('api/users/create/', UserCreateAPIView.as_view(), name="user-create"),
+    path('api/menu/', MenuListCreateAPIView.as_view(), name="menu"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
-from rest_framework.generics import CreateAPIView
+import datetime
+
+from rest_framework.generics import CreateAPIView, ListCreateAPIView
 from rest_framework.response import Response
-from .serializers import UserSerializer
+from api.serializers import UserSerializer, CreateMenuSerializer, MenuSerializer
 from django.contrib.auth import get_user_model
+from api.models import Menu
+from api.custom_permissions import IsRestaurantOnly
+from rest_framework import permissions
 
 
 User = get_user_model()
@@ -10,3 +15,33 @@ User = get_user_model()
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class MenuListCreateAPIView(ListCreateAPIView):
+    queryset = Menu.objects.filter(menu_date=datetime.date.today())
+    serializer_class = MenuSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateMenuSerializer
+        return self.serializer_class
+
+    def create(self, request, *args, **kwargs):
+        print(request.user.id)
+        if self.is_user_id_equals_request_user(user_id=request.data.get('user')):
+            if Menu.objects.filter(user=request.user, menu_date=datetime.date.today()).exists():
+                return Response(data={"error": "You already added menu today"})
+
+            return super().create(request, *args, **kwargs)
+        return Response(data={"error": "User id does not equals to request user id"})
+
+    def is_user_id_equals_request_user(self, user_id):
+        return int(user_id) == self.request.user.id
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            self.permission_classes = [permissions.IsAuthenticated, IsRestaurantOnly]
+        else:
+            self.permission_classes = [permissions.IsAuthenticated]
+
+        return [permission() for permission in self.permission_classes]
